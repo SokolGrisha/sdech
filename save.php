0dd0b5c9b29a4f120cd61e293a922718cfc6a91a
SokolGrisha <grsha9@gmail.com>
2018-07-03 17:49:11 +0300

    Add files via upload
    PHP path of sdech v1.2 waiting test
    Delete bugs: pyb and create directiry problem

--- save.php
+++ save.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET["tred"])&&isset($_GET["pyb"])){
-	file_put_contents("ch/".$_GET["tred"]."/"."pub.txt", $_GET["pyb"]);
+if(isset($_GET["tred"])&&isset($_GET["pub"])){
+	file_put_contents("./ch/".$_GET["tred"]."/"."pub.txt", $_GET["pub"]);
 	echo "Ok!";
 }  ?>
