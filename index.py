31e82ffa478a12ff37fe8f5314bcb08add1fccd3
SokolGrisha <grsha9@gmail.com>
2018-07-03 17:46:52 +0300

    Add files via upload
    sdech v1.2 python work and waiting test users
    Sorry,  065368d

--- index.py
+++ index.py
@@ -1,78 +1,75 @@
 import rsa
 import urllib.request
 import threading
-
-
-global pubf
-(pubu, privu) = rsa.newkeys(1024)
-
-
+## end import and start def
 def stru(txt):
-    txt=str(txt).split(" ")[0]+str(txt).split(" ")[1]
-    return txt
-
-
+        txt=str(txt).split(",")[0].split("PublicKey(")[1]
+        return txt
 def rsu(txt):
-    txt=str(txt).split(",")
-    txt=txt[0]+", "+txt[1]
-    return txt
-
-
-def writer(serv):
-    global lastmsg
-    while True:
-        global pubf
-        msg = rsa.encrypt(input("msg:"), pubf)
-        t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg)).read().decode("utf-8")
-        lastmsg = msg
+        txt=rsa.PublicKey(int(txt),65537)
+        return txt
+def writer(srv):
+        global lastmsg
+        while True:
+                import rsa
+                global pubf
+                msg = rsa.encrypt(input("msg:"), pubf)
+                t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg)).read().decode("utf-8")
+                lastmsg=msg
+                print(msg)
+def reader(srv):
+        global lastmsg
+        while True:
+                while msg!=lastmsg:
+                        msg=urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
+                        import rsa
+                        global pubu
+                        global privu
+                        msg = rsa.decrypt(t, privu)
         print(msg)
-
-
-def reader(serv):
-    global lastmsg
-    while True:
-        while msg!=lastmsg:
-            msg = urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
-            global pubu
-            global privu
-            msg = rsa.decrypt(t, privu)
-    print(msg)
-
-
-print("This file is needed to connect to server!")
-srv = str(input("Server:"))
-cc = input("Create\Connect ? cr\co")
-tred = input("Thread?")
-msg = input("Your first message:")
+## end def and start code
+global pubf #PublicKey you freind
+(pubu, privu) = rsa.newkeys(512)#create keys
+print("Здраствуйте! Это sdech.py! Он нужен для соедниния с сервером, надеюсь у вас есть сервер на котром уже готово!")
+srv=str(input("Сервер"))#Server
+cc=input("Create\Connect ? cr\co")
+tred=input("Tred?")#chat
 if(cc=="cr"):
-    t = urllib.request.urlopen(srv+"/creat.php?tred="+tred+"&pyb="+stru(pubu)).read().decode("utf-8")
-    if(t == "Ok!"):
-        print("Thread created!")
-        print("Waiting for friend's connection!")
-        while gey != str(pubu):
-            gey = urllib.request.urlopen(srv+"/ch/"+tred+"/pyb.txt").read().decode("utf-8")
-        print("Key received!")
-        pubf = gey
-        msg1 = rsa.encrypt(msg, pubf)
-        t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg1)).read().decode("utf-8")
-        print("Message sent!")
+        t = urllib.request.urlopen(srv+"/creat.php?tred="+tred+"&pub="+stru(pubu)).read().decode("utf-8")
+        if(t=="Ok!"):
+                print("Тред создан!")
+                print("Ждем подключение друга!")
+                wait=stru(pubu)
+                while(wait==stru(pubu)):
+                        wait=urllib.request.urlopen(srv+"/ch/"+tred+"/pub.txt").read().decode("utf-8")
+                print("Ключ получен!")
+                pubf=wait
+                msg1= rsa.encrypt("PubHave".encode(), pubf)
+                t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg1)).read().decode("utf-8")
+                print("Сообщили другу, что все ок!")
 else:
-    t = urllib.request.urlopen(srv+"/ch/"+tred+"/pyb.txt").read().decode("utf-8")
-    pubf = t
-    print("Key received!")
-    t = urllib.request.urlopen(srv+"/save.php?tred="+tred+"&pyb="+stru(pubu)).read().decode("utf-8")
-    if(t == "Ok!"):
-        print("Key sent!")
-        t = urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
-        msgf = rsa.decrypt(t, privu)
-        print("First message from friend:", msgf)
-while msg!="/end":
-        print("\nNote: /end is an end of message\n")
-        lastmsg = msg
-        # init threads
-        t1 = threading.Thread(target=writer, args=(serv, pubf))
-        t2 = threading.Thread(target=reader, args=(serv, privu))
+        t = urllib.request.urlopen(srv+"/ch/"+tred+"/pub.txt").read().decode("utf-8")
+        pubf=rsa.PublickKey(int(t),65537)
+        print("Ключ получен!")
+        t = urllib.request.urlopen(srv+"/save.php?tred="+tred+"&pub="+stru(pubu)).read().decode("utf-8")
+        if(t=="Ok!"):
+                print("Ключ отправлен!")
+                t = urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
+                msgf=rsa.decrypt(t, privu).decode()
+                if(msgf=="PubHave"):
+                	print("Ключ доставлен")
+# init threads
+t1 = threading.Thread(target=writer, args=(srv, pubf))
+t2 = threading.Thread(target=reader, args=(srv, privu))
 
-        # start threads
-        t1.start()
-        t2.start()
+print("")
+print("Памятка: /end окнчание разговора")
+print("")
+
+# start threads
+t1.start()
+t2.start()
+
+msg="Start!!"
+while msg!="/end":
+        lastmsg=msg
