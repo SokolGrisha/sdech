fe05a22bc387d01a5aa6c04b240f9938bf922b0e
SokolGrisha <grsha9@gmail.com>
2018-07-05 22:42:47 +0300

    Add files via upload
    python v2.0
    upd: delite requests and use only urllib; use new system for send encrypto msg
    bags: can't send BIG MSG

--- index.py
+++ index.py
@@ -1,32 +1,36 @@
 import rsa
 import urllib.request
+import requests
+
 ## end import and start def
+def by2st(byt):
+    ar=[]
+    for i in range(len(byt)):
+        ar.append(byt[i])
+    ans=''
+    for i in range(len(ar)):
+        if(i!=len(ar)-1):
+            ans+=str(ar[i])+","
+        else:
+            ans+=str(ar[i])
+    return ans
+def st2by(st):
+    st=st.split(',')
+    for i in range(len(st)):
+        st[i]=int(st[i])
+    return bytes(st)
+def ed(srv,tred,msg):
+    msg=by2st(msg)
+    results = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+msg).read().decode("utf-8")
+def cr(srv,tred,pub):
+    results = requests.get(srv+"creat.php", params={'tred': tred, 'pub': str(pub)})
 def stru(txt):
         txt=str(txt).split(",")[0].split("PublicKey(")[1]
         return txt
 def rsu(txt):
         txt=rsa.PublicKey(int(txt),65537)
         return txt
-#def writer(srv):
-#        global lastmsg
-#        while True:
-#                import rsa
-#                global pubf
-#                msg = rsa.encrypt(input("msg:"), pubf)
-#                t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg)).read().decode("utf-8")
-#                lastmsg=msg
-#                print(msg)
-#def reader(srv):
-#        global lastmsg
-#        while True:
-#                while msg!=lastmsg:
-#                        msg=urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
-#                        import rsa
-#                        global pubu
-#                        global privu
-#                        msg = rsa.decrypt(t, privu)
-#        print(msg)
-## end def and start code
+## end def
 global pubf #PublicKey you freind
 (pubu, privu) = rsa.newkeys(512)#create keys
 print("Здраствуйте! Это sdech.py! Он нужен для соедниния с сервером, надеюсь у вас есть сервер на котром уже готово!")
@@ -43,8 +47,9 @@ if(cc=="cr"):
                         wait=urllib.request.urlopen(srv+"/ch/"+tred+"/pub.txt").read().decode("utf-8")
                 print("Ключ получен!")
                 pubf=rsa.PublicKey(int(wait),65537)
-                t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(rsa.encrypt("PubHave".encode(), pubf))).read().decode("utf-8")
+                t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+by2st(rsa.encrypt("PubHave".encode(), pubf))).read().decode("utf-8")
                 print("Сообщили другу, что все ок!")
+                nin=0
 else:
         t = urllib.request.urlopen(srv+"/ch/"+tred+"/pub.txt").read().decode("utf-8")
         pubf=rsa.PublicKey(int(t),65537)
@@ -61,24 +66,30 @@ else:
                                 if i%100:
                                         print("Ждем подключение!")
                         try:
-                                msgf=rsa.decrypt(t.encode(), privu)
+                                msgf=rsa.decrypt(st2by(t), privu).decode()
                         except:
                                 msgf=msgf
                         i+=1
                 print("Ключ доставлен")
+        nin=1
 
 print("")
-print("Памятка: /end окнчание разговора")
+print("Памятка: /end окнчание разговора, пустое сообщения - пререзагрузка(проверка на новые сообщения)")
 print("")
-
-lm=t;
-msg="Start!!"
-while msg!="/end":
-        msgf=urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
-        try:
-                msgf= rsa.decrypt(msgf.encode(), privu)
-                print(msgf)
-        except:
-                msg=msg
-        msg = rsa.encrypt(input("msg").encode(), pubf)
-        t = urllib.request.urlopen(srv+"/edit.php?tred="+tred+"&msg="+str(msg)).read().decode("utf-8")
+if(nin==1):
+    msgfr=msgf
+    inp=msgfr
+else:
+    msgfr=""
+    inp=msgfr
+while True:
+    try:
+        inp=rsa.decrypt(st2by(inp), privu).decode()
+        if(inp!=msgfr):
+            print("f: "+inp)
+    except:
+        inp=inp
+    my=input("msg:")
+    msgfr=my
+    inp=urllib.request.urlopen(srv+"/ch/"+tred+"/msg.txt").read().decode("utf-8")
+    ed(srv,tred,rsa.encrypt(my.encode(),pubf))
