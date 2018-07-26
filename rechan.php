7b22d7d1746363078c3df25379eaf03adac580df
SokolGrisha <grsha9@gmail.com>
2018-07-26 18:53:11 +0300

    PHP path of channel
    Исправлены ошибки, теперь все работеат без проблем. Единственный недостаток это возможность уничтожения канала через пересоздания!

--- rechan.php
+++ rechan.php
@@ -1,9 +1,7 @@
 <?php
 if(isset($_GET["name"])&&isset($_GET["pas"])&&isset($_GET["msg"])){
 	if(file_get_contents("./ch/".$_GET["name"]."/"."pas.txt")==hash("sha256",$_GET["pas"])){
-		$fileopen=fopen("./ch/".$_GET["name"]."/"."lenta.txt", "a+");
-		fwrite($fileopen,$_GET["msg"]);
-		fclose($fileopen);
+		file_put_contents("./ch/".$_GET["name"]."/"."lenta.txt", $_GET["msg"]);
 		echo "Ok!";
 }
 }  ?>
