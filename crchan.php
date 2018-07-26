92f951be5aa296a85a6f921b1e6d79292dc5b467
SokolGrisha <grsha9@gmail.com>
2018-07-26 19:07:05 +0300

    PHP path of channel (SAVE U CHANNEL)
    Проблема с возможностью удаления канала решена!

--- crchan.php
+++ crchan.php
@@ -1,8 +1,13 @@
 <?php
 if(isset($_GET["name"])&&isset($_GET["pas"])&&isset($_GET["msg"])&&isset($_GET["pri"])){
+	if(file_exists("./ch/".$_GET["name"]."/"."key.txt")==false&&file_exists("./ch/".$_GET["name"]."/"."lenta.txt")==false&&file_exists("./ch/".$_GET["name"]."/"."pas.txt")==false){
 	mkdir('./ch/'.$_GET["name"]);
 	file_put_contents("./ch/".$_GET["name"]."/"."key.txt", $_GET["pri"]);
 	file_put_contents("./ch/".$_GET["name"]."/"."lenta.txt", $_GET["msg"]);
 	file_put_contents("./ch/".$_GET["name"]."/"."pas.txt", hash("sha256",$_GET["pas"]));
 	echo "Ok!";
+}
+else {
+	echo "Huei";
+}
 }  ?>
